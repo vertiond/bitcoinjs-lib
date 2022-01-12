e06fe2c17ff2ff33fc57841992013e61cae3a68b
vertiond <35286924+vertiond@users.noreply.github.com>
2022-01-12 00:48:52 -0600

    Vertcoin network parameters

--- src/networks.js
+++ src/networks.js
@@ -3,34 +3,34 @@ Object.defineProperty(exports, '__esModule', { value: true });
 exports.testnet = exports.regtest = exports.bitcoin = void 0;
 exports.bitcoin = {
   messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'bc',
+  bech32: 'vtc',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4,
   },
-  pubKeyHash: 0x00,
+  pubKeyHash: 0x47,
   scriptHash: 0x05,
   wif: 0x80,
 };
 exports.regtest = {
   messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'bcrt',
+  bech32: 'rvtc',
   bip32: {
     public: 0x043587cf,
     private: 0x04358394,
   },
-  pubKeyHash: 0x6f,
+  pubKeyHash: 0x4a,
   scriptHash: 0xc4,
   wif: 0xef,
 };
 exports.testnet = {
   messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'tb',
+  bech32: 'tvtc',
   bip32: {
     public: 0x043587cf,
     private: 0x04358394,
   },
-  pubKeyHash: 0x6f,
+  pubKeyHash: 0x4a,
   scriptHash: 0xc4,
   wif: 0xef,
 };
